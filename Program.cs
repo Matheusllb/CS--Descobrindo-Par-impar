41b64cd091daf3900e11b80d6fc26b508809b861
Matheusllb <matheusluislopesbatista@gmail.com>
2023-03-31 15:41:05 -0300

    Par/Impar

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CsE8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //EXERCÍCIO 8
+            Console.WriteLine("Descobrindo par/impar\n\nINFORME UM VALOR: ");
+            int v = int.Parse(Console.ReadLine());
+            //SE VALOR FOR DIVISIVEL POR 2 É PAR CASO CONTRARIO IMPAR
+            if (v % 2 == 0)
+                Console.WriteLine("\nNUMERO PAR");
+            else 
+                Console.WriteLine("\nNUMERO IMPAR");
+            Console.WriteLine("\nFim do Programa!\n");
+        }
+    }
+}
